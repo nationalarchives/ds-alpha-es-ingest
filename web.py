0d83aaa67fe7677495b77dc506caeed0434892e4
Matt McGrattan <matt.mcgrattan@digirati.com>
2020-02-25 13:32:32 +0000

    Make the connection to ILDB and to ES if the right variables are present.

--- web.py
+++ web.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify, abort, render_template
 from flask_cors import CORS
 from flask_executor import Executor
+import pyodbc
 import settings
+from elasticsearch import Elasticsearch
+import certifi
+
 
 app = Flask(__name__)
 CORS(app)
@@ -31,12 +35,42 @@ def ingest():
     es_index = request.args.get("es_index", default=settings.es_resolver_index)
     es_port = request.args.get("es_port", default=settings.es_port)
     es_host = request.args.get("es_host", default=settings.es_host)
+    action = bool(request.args.get("action", default=None))
     settings_dict ={"lettercode": lettercode, "start": start, "end": end, "es_index": es_index, "es_port": es_port,
                     "es_host": es_host,
+                    "ingest": action,
                     "settings.ildb": [settings.ildb_host,
                                       settings.ildb_port,
                                       settings.ildb_user,
                                       settings.ildb_password]}
+    if settings.ildb_host and settings.ildb_user and settings.ildb_password and settings.ildb_port:
+        ildb_connection = pyodbc.connect(
+            server=settings.ildb_host,
+            database="ILDB",
+            user=settings.ildb_user,
+            tds_version="7.4",
+            password=settings.ildb_password,
+            port=settings.ildb_port,
+            driver="FreeTDS",
+        )
+    else:
+        ildb_connection = None
+    if es_index and es_port and es_host:
+        es = Elasticsearch(
+            hosts=[
+                {
+                    "host": es_host,
+                    "use_ssl": True,
+                    "verify_certs": True,
+                    "port": es_port,
+                    "ca_certs": certifi.where(),
+                }
+            ]
+        )
+    else:
+        es = None
+    settings_dict["ildb_connection"] = bool(ildb_connection)
+    settings_dict["es_connection"] = bool(es)
     return jsonify(settings_dict)
 
 
