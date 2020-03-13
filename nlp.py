efeaf6dc35ce6bb556eab14ee3bb02fd75af7b7a
Matthew McGrattan <matthew.mcgrattan@bodleian.ox.ac.uk>
2020-03-13 12:31:44 +0000

    Try except on personalnames

--- nlp.py
+++ nlp.py
@@ -121,15 +121,19 @@ def string_to_entities(
                         else:  # Date parser couldn't identify the date, but we know it is one.
                             date_ents.append({"text": entity, "label": "DATE"})
                 elif ent.label_ == "PERSON":
-                    variants = names.name_initials(
-                        name=ent.text,
-                        name_formats=["firstnamelastname", "lastnamefirstname"],
-                    )
+                    try:
+                        variants = names.name_initials(
+                            name=ent.text,
+                            name_formats=["firstnamelastname", "lastnamefirstname"],
+                        )
+                        sorted_v = sorted(variants)
+                    except IndexError or KeyError or ValueError:
+                        sorted_v = None
                     name_ents.append(
                         {
                             "text": ent.text,
                             "label": ent.label_,
-                            "variants": sorted(variants),
+                            "variants": sorted_v,
                         }
                     )
                 else:  # Just iterate the entities
@@ -143,120 +147,3 @@ def string_to_entities(
         }
     return
 
-
-doc = {
-    "letter_code": "FO",
-    "division_no": 1,
-    "class_no": 371,
-    "subclass_no": None,
-    "class_hdr_no": 492,
-    "subheader_no": None,
-    "piece_ref": "85611",
-    "first_date": "1950-01-01",
-    "last_date": "1950-12-31",
-    "title": "Fusion Agreement: US-USA-French trizonal arrangements: drain on Germany's sterling; trizone drawing rights; OEEC Joint Trade and Intra-European Payments Committee. Code CE file 24 (papers 739 - 3444)",
-    "series": "371",
-    "path": {
-        "Department": "FO",
-        "Division": 1,
-        "Series": "371",
-        "Subseries": 492,
-        "Subsubseries": None,
-        "Piece": "85611",
-        "Item": None,
-    },
-    "level": "Piece",
-    "catalogue_ref": "FO 371/85611",
-    "id": "FO:~1:371:~492:85611",
-    "matches": [
-        "FO 371/85611",
-        "FO:~1:371:~492:85611",
-        "FO/371/85611",
-        "FO/~1/371/~492/85611",
-        "FO:371:85611",
-    ],
-    "first_date_obj": {"year": 1950, "month": 1, "day": 1, "century": 19},
-    "last_date_obj": {"year": 1950, "month": 12, "day": 31, "century": 19},
-    "eras": ["postwar"],
-    "research_guides": {
-        "Object": [],
-        "Department": [
-            6,
-            23,
-            29,
-            35,
-            42,
-            71,
-            93,
-            102,
-            107,
-            112,
-            118,
-            151,
-            158,
-            267,
-            270,
-        ],
-        "Series": [23, 33, 35, 78, 104, 112, 120, 165, 182, 183, 226, 253],
-        "All": [
-            6,
-            23,
-            29,
-            33,
-            35,
-            42,
-            71,
-            78,
-            93,
-            102,
-            104,
-            107,
-            112,
-            118,
-            120,
-            151,
-            158,
-            165,
-            182,
-            183,
-            226,
-            253,
-            267,
-            270,
-        ],
-    },
-    "mongo": {
-        "browse_parent_iaid": "C7685",
-        "covering_from_date": 19500101,
-        "covering_to_date": 19501231,
-        "catalogue_id": 3594139,
-        "closure": {"closure_code": "30", "closure_status": "O", "closure_type": "N"},
-        "covering_dates": "1950",
-        "former_reference_dept": "File 24 (papers 739-3444)",
-        "held_by": [
-            {
-                "archon_no": "66",
-                "archon_iaid": "A13530124",
-                "corporate_body_name": "The National Archives, Kew",
-            }
-        ],
-        "iaid": "C2843090",
-        "legal_status": "Public Record(s)",
-        "catalogue_level_id": 6,
-        "parent_iaid": "C58286",
-        "pref": "85611",
-        "reference": "FO 371/85611",
-        "scope_and_content": {
-            "description": "<scopecontent><p>Fusion Agreement: US-USA-French trizonal arrangements: drain on Germany's sterling; trizone drawing rights; OEEC Joint Trade and Intra-European Payments Committee. Code CE file 24 (papers 739 - 3444)</p></scopecontent>"
-        },
-        "sort_key": "06 0 785441000 0#85441000",
-    },
-    "iaid": "C2843090",
-}
-
-
-print(
-    string_to_entities(
-        input_string=flatten_to_string(input_obj=doc), nlp=spacy.load("en_core_web_sm")
-    )
-)
